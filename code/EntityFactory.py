28564d9aabb77e3e8773b290a060dfed9e64a26c
Enéias <tcciesgoeneias@gmail.com>
2026-04-03 16:17:50 -0300

    Adding classes generated from StarUML

---
+++ code/EntityFactory.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Level import Level
+
+
+class EntityFactory(Level):
+    def __init__(self):
+        pass
+
+    def get_entity(self, entity_type):
+        pass
---
+++ code/Player.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from EntityFactory import EntityFactory
+from Entity import Entity
+
+
+class Player(EntityFactory, Entity):
+    def __init__(self):
+        pass
+
+    def move(self, ):
+        pass
