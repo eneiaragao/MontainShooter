05b500ed1d6fddbc19175ca2c6e577d446057125
Enéias <tcciesgoeneias@gmail.com>
2026-04-06 21:02:48 -0300

    Creating Players an enemies Moving players and enemies

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+import random
 from code.Background import Background
-from code.const import WIN_WIDTH
+from code.Enemy import Enemy
+from code.Player import Player
+from code.const import WIN_WIDTH, WIN_HEIGHT
 
 
 class EntityFactory: # Removida a herança de Level
@@ -17,3 +19,11 @@ class EntityFactory: # Removida a herança de Level
                     list_bg.append(Background(f'Level1Bg{i}', (0,0)))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
+            case 'Player1':
+                return Player('Player1', (10,WIN_HEIGHT/2 -30))
+            case 'Player2':
+                return Player('Player2', (10, WIN_HEIGHT / 2 +30))
+            case 'Enemy1':
+                return Enemy("Enemy1",(WIN_WIDTH+10, random.randint(40,WIN_HEIGHT-40)))
+            case 'Enemy2':
+                return Enemy("Enemy2", (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT-40)))
--- code/Level.py
+++ code/Level.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import random
 import sys
+from random import choice
 
 import pygame.display
 from pygame import Surface, Rect
@@ -8,7 +10,7 @@ from pygame.font import Font
 
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
-from code.const import COLOR_WHITE, WIN_HEIGHT
+from code.const import COLOR_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
 
 
 class Level:
@@ -16,36 +18,44 @@ class Level:
         self.window = window
         self.name = name
         self.game_mode = game_mode
-        self.entity_list:list[Entity] = []
+        self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
-        self.timeout=2000 #20 segundos
+        self.entity_list.append(EntityFactory.get_entity('Player1'))  #adiciona nave player 1
+        self.timeout = 2000  #20 segundos
+
+        if game_mode in [MENU_OPTION[1], MENU_OPTION[2]]:  #ESCOLHENDO O PLAYER 2
+            self.entity_list.append(EntityFactory.get_entity('Player2'))  # adiciona nave player 2
+        # A CADA X TEMPO CRIA INIMIGO
+        pygame.time.set_timer(EVENT_ENEMY, SPAWN_TIME)
 
     def run(self, ):
         pygame.mixer.music.load(f'./asset/{self.name}.mp3')
         pygame.mixer_music.play(-1)
-        clock=pygame.time.Clock()
+        clock = pygame.time.Clock()
         while True:
             clock.tick(60)
             for ent in self.entity_list:
-                self.window.blit(source=ent.surf,dest=ent.rect)
+                self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
-            for event in pygame.event.get(): #EVENTO PARA FECHAR A JANELA DO JOGO
+            for event in pygame.event.get():  #EVENTO PARA FECHAR A JANELA DO JOGO
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == EVENT_ENEMY:#adiciona na lista  o inimigo
+                    choice = random.choice(('Enemy1','Enemy2'))
+                    self.entity_list.append(EntityFactory.get_entity(choice))
 
-               #TEXTOS DOS NÍVEIS (LEVEL)
+            #TEXTOS DOS NÍVEIS (LEVEL)
 
-
-            self.level_text(14,f'{self.name} - Timeout:{self.timeout / 1000:.1f}',COLOR_WHITE,(10,5))
-            self.level_text(14,f'fps: {clock.get_fps():.0f}',COLOR_WHITE,(10,WIN_HEIGHT -35))
-            self.level_text(14,f'entidades: {len(self.entity_list)}',COLOR_WHITE,(10,WIN_HEIGHT-20))
+            self.level_text(14, f'{self.name} - Timeout:{self.timeout / 1000:.1f}', COLOR_WHITE, (10, 5))
+            self.level_text(14, f'fps: {clock.get_fps():.0f}', COLOR_WHITE, (10, WIN_HEIGHT - 35))
+            self.level_text(14, f'entidades: {len(self.entity_list)}', COLOR_WHITE, (10, WIN_HEIGHT - 20))
             pygame.display.flip()
 
             pass
 
-    def level_text(self,text_size:int,text:str,text_color:tuple,text_pos:tuple):
-        text_font:Font = pygame.font.SysFont(name='Lucila Sans Typewriter', size=text_size)
-        text_surf:Surface = text_font.render(text, True, text_color).convert_alpha()
-        text_rect:Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name='Lucila Sans Typewriter', size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
         self.window.blit(source=text_surf, dest=text_rect)
